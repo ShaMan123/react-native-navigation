f00dfeadce96459a6843df0cc8e04f8da3934d2d
ShaMan123 <shacharnen@gmail.com>
2019-03-05 12:02:06 +0200

    Update package.json

---
+++ lib/src/interfaces/index.ts
@@ -0,0 +1,6 @@
+import * as ComponentEvents from './ComponentEvents';
+import * as Events from './Events';
+import * as EventSubscription from './EventSubscription';
+import * as Layout from './Layout';
+import * as Options from './Options';
+export { ComponentEvents, Events, EventSubscription, Layout, Options };
